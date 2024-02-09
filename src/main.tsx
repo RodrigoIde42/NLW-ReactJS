5c6530b0964e0b30678ee58fa779bfaa88c75db2
RodrigoIde42 <rodriigo.ide@gmail.com>
2024-02-08 23:39:29 -0300

    NLW React - third lesson

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './app.tsx'
 import { Toaster } from 'sonner'
 import './index.css'
 
-ReactDOM.createRoot(document.body!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <Toaster richColors />
